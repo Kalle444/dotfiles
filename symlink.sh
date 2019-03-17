d9f670424af79115cf05702ca2d0612d49d7422a
Karl Bergstrom <karl.bergstrom@karnovgroup.com>
2019-03-17 12:09:42 +0100

    use zshrc instead of bash

--- symlink.sh
+++ symlink.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/zsh
 
 echo "symlinking .files"
 
@@ -6,8 +6,10 @@ working_dir=$PWD
 
 echo "Working directory" $working_dir
 
-echo "Installing bash_profile"
-ln -sv $working_dir/.bash_profile ~/.bash_profile
+# echo "Installing bash_profile"
+# ln -sv $working_dir/.bash_profile ~/.bash_profile
+echo "Installing zshrc"
+ln -sv $working_dir/.zshrc ~/.zshrc
 echo "Installing gitconfig"
 ln -sv $working_dir/.gitconfig ~/.gitconfig
 echo "Installing irbrc"
